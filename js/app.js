4d98c640fb8a11997a2a6f5c0bc7ef190016146c
petewhartnett <petewhartnett@outlook.com>
2019-11-07 15:30:04 -0800

    functions added/REAMME

--- js/app.js
+++ js/app.js
@@ -1,50 +1,70 @@
 'use strict';
 
 //Lab 2
+function question1() {
 
-var userName = prompt('What is your name?');
+  var userName = prompt('What is your name?');
 
-var pets = prompt('Do I have any pets? Answer with yes or no');
-// console.log('Number of pets: ', pets);
-if (pets.toLowerCase() === 'y' || pets.toLowerCase() === 'yes') {
-  alert('That is incorrect I currently have not pets');
-} else {
-  alert('Correct I currently have no pets');
+  var pets = prompt('Do I have any pets? Answer with yes or no');
+  // console.log('Number of pets: ', pets);
+  if (pets.toLowerCase() === 'y' || pets.toLowerCase() === 'yes') {
+    alert('That is incorrect I currently have not pets');
+  } else {
+    alert('Correct I currently have no pets');
+  }
 }
 
-var drive = prompt('Guess how many times I\'ve driven across the country');
-// console.log('Times driven cross country: ', drive);
-if (drive === '4' || drive === 'four') {
-  alert('Correct I\'ve driven cross-country four times');
-} else {
-  alert('That is incorrect I have driven cross-country four times');
+function question2(){
+  var drive = prompt('Guess how many times I\'ve driven across the country');
+  // console.log('Times driven cross country: ', drive);
+  if (drive === '4' || drive === 'four') {
+    alert('Correct I\'ve driven cross-country four times');
+  } else {
+    alert('That is incorrect I have driven cross-country four times');
+  }
 }
 
-var status = prompt('Am I married? Answer with yes or no');
-// console.log('Status: ', status);
-if (status.toLowerCase() === 'y' || status.toLowerCase() === 'yes') {
-  alert('That is incorrect I am single');
-} else {
-  alert('Correct I\'m single');
+function question3(){
+  var status = prompt('Am I married? Answer with yes or no');
+  // console.log('Status: ', status);
+  if (status.toLowerCase() === 'y' || status.toLowerCase() === 'yes') {
+    alert('That is incorrect I am single');
+  } else {
+    alert('Correct I\'m single');
+  }
 }
 
-var boards = prompt('Guess how many surfboards I own');
-// console.log('Number of surfboards: ', boards);
-if (boards === '3' || boards.toLowerCase() === 'three') {
-  alert('Correct I own 3 surfboards');
-} else {
-  alert('That is incorrect I own 3 surfboards');
+function question4(){
+  var boards = prompt('Guess how many surfboards I own');
+  // console.log('Number of surfboards: ', boards);
+  if (boards === '3' || boards.toLowerCase() === 'three') {
+    alert('Correct I own 3 surfboards');
+  } else {
+    alert('That is incorrect I own 3 surfboards');
+  }
 }
 
-var birthplace = prompt('Guess where I was born');
-// console.log('Birthplace: ', birthplace);
-if (birthplace.toLowerCase() === 'philippines' || birthplace.toLowerCase() === 'the philippines') {
-  alert('Correct that is where I was born');
-} else {
-  alert('Incorrect I was not born in ' + birthplace);
+function question5(){
+  var birthplace = prompt('Guess where I was born');
+  // console.log('Birthplace: ', birthplace);
+  if (birthplace.toLowerCase() === 'philippines' || birthplace.toLowerCase() === 'the philippines') {
+    alert('Correct that is where I was born');
+  } else {
+    alert('Incorrect I was not born in ' + birthplace);
+  }
 }
+
+question1();
+question2();
+question3();
+question4();
+question5();
+
+
+
 
 //Lab 3
+
 
 var number = 25;
 for (var i = 0; i < 4; i++) {
@@ -68,7 +88,7 @@ function check(fruit) {
     if (fruit === fruit[i]) {
       correct++;
       //Prevent hacking of score
-      fruits.splice(i,0);
+      fruits.splice(i, 0);
 
       return true;
     }
@@ -82,3 +102,4 @@ for (var i = 0; i < 6; i++) {
 }
 
 document.getElementById('user_name').innerHTML = userName;
+
